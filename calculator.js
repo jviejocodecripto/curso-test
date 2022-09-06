10f2aea2e474df9a7daa006037b751d7f62ca5e2
jviejocodecripto <105506931+jviejocodecripto@users.noreply.github.com>
2022-09-06 18:01:41 +0200

    Feature (#1)
    * cambiios
    
    * feature b
    
    * feature b mas
    
    Co-authored-by: jvh <jviejo@gmail.com>

--- calculator.js
+++ calculator.js
@@ -1,8 +1,9 @@
 const suma =  (a, b) => a+b
 const mul =  (a, b) => a*b
-const div =  (a, b) => null
-
-
+const div =  (a, b) => a/b
+// cambios
+// cambios feature
+// mas cambios
 module.exports = {
     suma,
     mul,
